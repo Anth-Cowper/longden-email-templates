b6b5b416d519e19b9649e9ea4b33f6b2d94a0f8b
Anth-Cowper <anth.cowper@longdendoors.co.uk>
2026-07-03 13:04:22 +0100

    Fix editor: retry fetch on load, fix placeholder regex, rebrand to black

--- start-editor.bat
+++ start-editor.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
 start /min "Email Templates Editor" "C:\Program Files\nodejs\node.exe" server.js
-timeout /t 1 /nobreak >nul
+timeout /t 3 /nobreak >nul
 start http://localhost:3457
